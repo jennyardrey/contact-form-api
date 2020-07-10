0274a640c1cf6fc2f5e9d5e5e13e7475be178a0a
Jenny Ardrey <jenny@ardrey.co.uk>
2020-07-10 14:31:47 +0100

    added procfile and altered package for deployment

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const nodemailer = require('nodemailer');
-const creds = require('./config');
+const creds = require('./config.js');
 
 const app = express();
 app.use(cors());
